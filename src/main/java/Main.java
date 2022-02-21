96d5da92e90a0f200fb71af4d808caed4f1218a5
Andres Matos <andresmatos2000@gmail.com>
2022-02-20 20:37:24 -0700

    Fix Main.java name to work with Dockerfile

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
